73172ad00d83ad67fe9b0f71e77e8c02f24faccb
Sean Perkins <sean@ionic.io>
2022-06-03 14:00:03 -0400

    chore(): standalone component

--- src/app/standalone.component.ts
+++ src/app/standalone.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-standalone',
-  template: `<p>This is a standalone component.`,
+  template: `<ion-button>Standalone Button</ion-button>`,
+  imports: [IonicModule],
   standalone: true
 })
 export class StandaloneComponent { }
